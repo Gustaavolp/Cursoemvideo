02c34f12933614a3e731d553b66b7a96f361dc2d
Gustavo Liebl Pereira <gustavolp2005@gmail.com>
2023-03-19 20:15:57 -0300

    Pintando Parede

--- ex011.py
+++ ex011.py
@@ -1,4 +1,7 @@
-al = int(input('Digite a altura de sua parede: '))
-lar = int(input('Digite a largura de sua parede: '))
-at = int(al*lar)
-print('A área total de sua parede é de {}m² e usará aproximadamente {} litros de tinta'.format(at, at/2))
+#Faça um programa que leia a altura e a largura de uma parede em metros, 
+#Calcule a sua área e a quantidade de tinta nescessária para pintá-la, 
+#Sabendo que cada litro de tinta, pinta uma área de 2m².
+alt = float(input('Digite a altura de sua parede: '))
+larg = float(input('Digite a largura de sua parede: '))
+área = alt * larg
+print('A área total de sua parede é de {}m² e usará aproximadamente {}l de tinta'.format(área, área/2))
