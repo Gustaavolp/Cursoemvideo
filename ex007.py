00acc783af8097421c8ccbab123f91dbb9abf1fe
Gustavo Liebl Pereira <gustavolp2005@gmail.com>
2023-03-19 19:20:37 -0300

    Média

--- ex007.py
+++ ex007.py
@@ -1,4 +1,5 @@
-n1 = int(input('Digite a primira nota: '))
-n2 = int(input('Digite a segunda nota: '))
+#desenvolva um programa que leia as duas notas de um alno, calcule e mostre a sua média.
+n1 = float(input('Digite a primira nota: '))
+n2 = float(input('Digite a segunda nota: '))
 m = ((n1+n2)/2)
-print('A média entre as notas digitadas foi: {}'.format(m))
+print('A média entre {:.1f} e {:.1f} é igual a {:.1f}'.format(n1, n2, m))
