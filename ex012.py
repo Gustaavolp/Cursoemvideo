aeab18d5683a8b67c7a43450ecaf21d9b71f1d39
Gustavo Liebl Pereira <gustavolp2005@gmail.com>
2023-03-19 20:38:38 -0300

    Calculando Descontos

--- ex012.py
+++ ex012.py
@@ -1,2 +1,3 @@
-pp = float(input('Digite o preço de seu produto: '))
-print('O novo valor de seu produto com desconto de 5% sera de: {}'.format(pp-(5/100*pp)))
+#Faça um algoritmo que leia o preço de um produto e mostre seu novo preço, com 5% de desconto.
+preço = float(input('Digite o preço de seu produto R$:'))
+print('O novo valor de seu produto de R$:{:.2f} com desconto de 5% sera de R$:{:.2f}'.format(preço, preço-(preço*5/100)))
