e4a791bda2af71b85d01f840cb11a3af12c455fd
Gustavo Liebl Pereira <gustavolp2005@gmail.com>
2023-03-19 19:42:59 -0300

    Conversor de Medidas

--- ex008.py
+++ ex008.py
@@ -1,5 +1,3 @@
 #Escreva um programa que leia um valor em metros e o exiba convertido em centímetros e milímetros.
 m = float(input('Digite um valor em metros: '))
-cm = m*100
-mm = m*1000
-print('A medida de {}m corresponde a {:.0f}cm e {:.0f}mm'.format(m, cm, mm))
+print('A medida de {}m corresponde a {:.0f}cm e {:.0f}mm'.format(m, m*100, m*1000))
