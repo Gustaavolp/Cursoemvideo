2cd12762330b8ddcfd515c78b472ee16c720215f
Gustavo Liebl Pereira <gustavolp2005@gmail.com>
2023-03-19 20:05:40 -0300

    Conversor de Moedas

--- ex010.py
+++ ex010.py
@@ -1,2 +1,4 @@
-rs = float(input('Quantos reais voce tem?: '))
-print('Com {} reais voce podera comprar {:.2f} dolares'.format(rs, rs/3.27))
+#Crie um programa que leia quanto dinheiro uma pessoa tem na carteira e mostre quantos Dólares ela pode comprar.
+#Considere US$:1,00 = R$:3,27
+rs = float(input('Quantos reais voce tem? R$:'))
+print('Com R$:{:.2f} voce pode comprar US$:{:.2f}'.format(rs, rs/3.27))
