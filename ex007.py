22805a648fd6eca66523e13c1c8df9d2cba0fa3e
Gustavo Liebl Pereira <gustavolp2005@gmail.com>
2023-03-19 19:22:51 -0300

    Média Aritmética

--- ex007.py
+++ ex007.py
@@ -1,5 +1,5 @@
 #desenvolva um programa que leia as duas notas de um alno, calcule e mostre a sua média.
-n1 = float(input('Digite a primira nota: '))
+n1 = float(input('Digite a primeira nota: '))
 n2 = float(input('Digite a segunda nota: '))
 m = ((n1+n2)/2)
 print('A média entre {:.1f} e {:.1f} é igual a {:.1f}'.format(n1, n2, m))
