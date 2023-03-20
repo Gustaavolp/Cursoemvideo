01d84c3268145dc776d5c3bd9c85fabe71a71956
Gustavo Liebl Pereira <gustavolp2005@gmail.com>
2023-03-19 21:14:25 -0300

    Reajuste Salarial

--- ex013.py
+++ ex013.py
@@ -1,2 +1,3 @@
-s = int(input('Digite seu salário: '))
-print('Seu novo salário sera de R$:{:.2f}'.format(s+(15/100*s)))
+#Faça um algoritmo que leia o salário de um funcionario e mostre seu novo salário, com 15% de aumento.
+s = float(input('Digite seu salário: '))
+print('Seu novo salário será de R$:{:.2f}'.format(s+(s*15/100)))
